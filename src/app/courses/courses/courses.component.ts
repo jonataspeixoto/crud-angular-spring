4cb35106722a6f329eac4ae6fd376453d1f313f4
jonataspeixoto <jonatas_micael@hotmail.com>
2023-09-17 16:09:18 -0300

    Componente de Formulário e Roteamento para criar cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -4,6 +4,7 @@ import { CoursesService } from '../services/courses.service';
 import { Observable, catchError, of } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -12,13 +13,14 @@ import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/err
 })
 export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['name', 'category', 'actions'];
 
   constructor(
     private coursesService: CoursesService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
   ) {
-    //this.coursesService = new CoursesService();
     this.courses$ = this.coursesService.list().pipe(
       catchError((error) => {
         this.onError('Erro ao carregar cursos.')
@@ -34,4 +36,16 @@ export class CoursesComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  onAdd(){
+    this.router.navigate(['new'], {relativeTo: this.route})
+  }
+  onEdit(){
+    //this.router.navigate(['new'], {relativeTo: this.route})
+    console.log('onEdit');
+  }
+  onDelete(){
+    //this.router.navigate(['new'], {relativeTo: this.route})
+    console.log('onDelete');
+  }
 }
