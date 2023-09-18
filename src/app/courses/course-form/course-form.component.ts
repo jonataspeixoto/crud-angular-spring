567c97ce93e321439bceb3aa943771a7c360d40f
jonataspeixoto <jonatas_micael@hotmail.com>
2023-09-17 23:00:45 -0300

    Formulário: API Spring: Criar Curso (HTTP POST)

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { CoursesService } from '../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-course-form',
@@ -11,7 +12,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class CourseFormComponent implements OnInit {
 
   onCancel() {
-    console.log("onCancel");
+    this.location.back();
   }
   onSubmit() {
     this.service.save(this.form.value).subscribe(result => console.log(result), error => this.onError());
@@ -19,12 +20,18 @@ export class CourseFormComponent implements OnInit {
   onError(){
     this._snackBar.open('Erro ao salvar curso', '', { duration: 5 * 1000 });
   }
+  onSuccess(){
+    this._snackBar.open('Curso salvo com sucesso', '', { duration: 5 * 1000 });
+    this.onCancel();
+  }
+
   form: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private _snackBar: MatSnackBar) {
+    private _snackBar: MatSnackBar,
+    private location: Location) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null],
