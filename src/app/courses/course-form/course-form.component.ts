2ce6f52d5ac65589898fff8f82cddbf779e9cc2a
jonataspeixoto <jonatas_micael@hotmail.com>
2023-09-23 12:58:15 -0300

    Angular Typed Forms

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { CoursesService } from '../services/courses.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NonNullableFormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -11,11 +11,23 @@ import { Location } from '@angular/common';
 })
 export class CourseFormComponent implements OnInit {
 
+  form = this.formBuilder.group({
+    name: [''],
+    category: [''],
+  });
+
+  constructor(
+    private formBuilder: NonNullableFormBuilder,
+    private service: CoursesService,
+    private _snackBar: MatSnackBar,
+    private location: Location) {
+  }
+
   onCancel() {
     this.location.back();
   }
   onSubmit() {
-    this.service.save(this.form.value).subscribe(result => console.log(result), error => this.onError());
+    this.service.save(this.form.value).subscribe(result => this.onSuccess(), error => this.onError());
   }
   onError(){
     this._snackBar.open('Erro ao salvar curso', '', { duration: 5 * 1000 });
@@ -23,19 +35,6 @@ export class CourseFormComponent implements OnInit {
   onSuccess(){
     this._snackBar.open('Curso salvo com sucesso', '', { duration: 5 * 1000 });
     this.onCancel();
-  }
-
-  form: FormGroup;
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private service: CoursesService,
-    private _snackBar: MatSnackBar,
-    private location: Location) {
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null],
-    });
   }
 
   ngOnInit(): void {}
